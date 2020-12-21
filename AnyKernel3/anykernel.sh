d44c049009d1b6d7244433dcb0e7874c6a055ba0
CaliBerrr <mominulislamgit01@gmail.com>
2020-12-21 17:20:25 +0000

    Initial Commit
    Uploading Source

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,146 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+# Customized for NetHunter
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=NetHunter Kernel for Mi A3 by CaliBerrr @ github
+do.devicecheck=1 #Use value 1 while using boot-patcher standalone
+do.modules=1
+do.systemless=0#Never use this for NetHunter kernels as it prevents us from writing to /lib/modules
+do.cleanup=0
+do.cleanuponabort=0
+device.name1=laurel_sporut
+device.name2=laurus
+device.name3=
+device.name4=
+device.name5=
+device.name6=
+device.name7=
+device.name8=
+device.name9=
+device.name10=
+supported.versions=9
+'; } # end properties
+
+# shell variables
+
+#NetHunter Addition
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## NetHunter additions
+
+SYSTEM="/system";
+SYSTEM_ROOT="/system_root";
+
+setperm() {
+	find "$3" -type d -exec chmod "$1" {} \;
+	find "$3" -type f -exec chmod "$2" {} \;
+}
+
+install() {
+	setperm "$2" "$3" "$home$1";
+	if [ "$4" ]; then
+		cp -r "$home$1" "$(dirname "$4")/";
+		return;
+	fi;
+	cp -r "$home$1" "$(dirname "$1")/";
+}
+
+[ -d $home/system/etc/firmware ] && {
+        ui_print "- Copying firmware to $SYSTEM/etc/firmware"
+	install "/system/etc/firmware" 0755 0644 "$SYSTEM/etc/firmware";
+}
+
+[ -d $home/system/etc/init.d ] && {
+        ui_print "- Copying init.d scripts to $SYSTEM/etc/init.d"
+	install "/system/etc/init.d" 0755 0755 "$SYSTEM/etc/init.d";
+}
+
+[ -d $home/system/lib ] && {
+        ui_print "- Copying 32-bit shared libraries to ${SYSTEM}/lib"
+	install "/system/lib" 0755 0644 "$SYSTEM/lib";
+}
+
+[ -d $home/system/lib64 ] && {
+        ui_print "- Copying 64-bit shared libraries to ${SYSTEM}/lib"
+	install "/system/lib64" 0755 0644 "$SYSTEM/lib64";
+}
+
+[ -d $home/system/bin ] && {
+        ui_print "- Installing ${SYSTEM}/bin binaries"
+	install "/system/bin" 0755 0755 "$SYSTEM/bin";
+}
+
+[ -d $home/system/xbin ] && {
+        ui_print "- Installing ${SYSTEM}/xbin binaries"
+	install "/system/xbin" 0755 0755 "$SYSTEM/xbin";
+}
+
+[ -d $home/data/local ] && {
+        ui_print "- Copying additional files to /data/local"
+	install "/data/local" 0755 0644;
+}
+[ -d $home/vendor/etc/init ] && {
+        mount /vendor;
+        chmod 644 $home/vendor/etc/init/*;
+	cp -r $home/vendor/etc/init/* /vendor/etc/init/;
+}
+[ -d $home/ramdisk-patch ] && {
+	setperm "0755" "0750" "$home/ramdisk-patch";
+        chown root:shell $home/ramdisk-patch/*;
+	cp -rp $home/ramdisk-patch/* "$SYSTEM_ROOT/";
+}
+
+if [ ! "$(grep /init.nethunter.rc $SYSTEM_ROOT/init.rc)" ]; then
+  insert_after_last "$SYSTEM_ROOT/init.rc" "import .*\.rc" "import /init.nethunter.rc";
+fi;
+
+if [ ! "$(grep /dev/hidg* $SYSTEM_ROOT/ueventd.rc)" ]; then
+  insert_after_last "$SYSTEM_ROOT/ueventd.rc" "/dev/kgsl.*root.*root" "# HID driver\n/dev/hidg* 0666 root root";
+fi;
+
+ui_print "- Applying additional anykernel installation patches";
+for p in $(find ak_patches/ -type f); do
+  ui_print "- Applying $p";
+  . $p;
+done;
+
+## End NetHunter additions
+
+
+## AnyKernel file attributes
+##set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+
+## AnyKernel install
+dump_boot;
+
+
+ui_print "- Patching Ramdisk";
+# begin ramdisk changes
+
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
+
+if [ -d $ramdisk/.backup ]; then
+  patch_cmdline "skip_override" "skip_override";
+else
+  patch_cmdline "skip_override" "";
+fi;
+
+# end ramdisk changes
+
+write_boot;
+## end install
+
